a39a5e87df6f71191a8fd9e3f7888b575e3a9f02
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2024-02-21 16:10:52 +0900

    소스 파일

---
+++ bookstore/src/App.tsx
@@ -0,0 +1,18 @@
+import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import { GlobalStyle } from './style/global';
+import { ThemeProvider } from 'styled-components';
+import { dark, light } from './style/theme';
+
+function App() {
+  return (
+    <ThemeProvider theme={dark}>
+      <GlobalStyle themeName='dark' />
+      <Layout>
+        <Home />
+      </Layout>
+    </ThemeProvider>
+  );
+}
+
+export default App;
